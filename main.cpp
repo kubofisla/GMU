0cfff3615b9fc46c7175e952489fa547470ac058
kubofisla <kubofisla@gmail.com>
2014-12-29 17:24:30 +0100

    perlin noise beta

--- main.cpp
+++ main.cpp
@@ -13,7 +13,9 @@
 #define WIN_SIZE_H 600
 
 #define MAP_SIZE 512
-#define ITERATIONS 1
+#define ITERATIONS 256
+
+#define STEP 1
 
 # define GLUT_WHEEL_UP 3
 # define GLUT_WHEEL_DOWN 4
@@ -256,6 +258,25 @@ void mouseMove(int x, int y) {
 	}
 }
 
+void Keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 27:             // ESCAPE key
+		exit(0);
+		break;
+
+	case '+':
+		tz += 1;
+		break;
+
+	case '-':
+		tz -= 1;
+		break;
+	}
+}
+
+
 void cpuFaultAlgorithm(float **matrix, int w, int l, int iterationCount)
 {
     float v;
@@ -301,6 +322,79 @@ void gpuFaultAlgorithm(float **matrix, int w, int l, int iterationCount)
 }
 
 
+float Cosine_Interpolate(float a, float b, float x)
+{
+	float ft = x * 3.1415927;
+	float f = (1 - cos(ft)) * .5;
+
+	return  a*(1 - f) + b*f;
+}
+
+//http://freespace.virgin.net/hugo.elias/models/m_perlin.htm
+float PseudoRandom_NoiseI(int x, int y)
+{
+	int n = x + y * 57;
+	n = (n << 13) ^ n;
+	return (1.0 - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0);
+}
+
+float Smooth_NoiseI(float x, float y)
+{
+	float corners = (PseudoRandom_NoiseI(x - 1, y - 1) + PseudoRandom_NoiseI(x + 1, y - 1) + PseudoRandom_NoiseI(x - 1, y + 1) + PseudoRandom_NoiseI(x + 1, y + 1)) / 16;
+	float sides = (PseudoRandom_NoiseI(x - 1, y) + PseudoRandom_NoiseI(x + 1, y) + PseudoRandom_NoiseI(x, y - 1) + PseudoRandom_NoiseI(x, y + 1)) / 8;
+	float center = PseudoRandom_NoiseI(x, y) / 4;
+	return corners + sides + center;
+}
+
+float Interpolated_NoiseI(float x, float y){
+	int integer_X = int(x);
+	float fractional_X = x - integer_X;
+
+	int integer_Y = int(y);
+	float fractional_Y = y - integer_Y;
+
+	float v1 = PseudoRandom_NoiseI(integer_X, integer_Y);
+	float v2 = PseudoRandom_NoiseI(integer_X + 1, integer_Y);
+	float v3 = PseudoRandom_NoiseI(integer_X, integer_Y + 1);
+	float v4 = PseudoRandom_NoiseI(integer_X + 1, integer_Y + 1);
+
+	float i1 = Cosine_Interpolate(v1, v2, fractional_X);
+	float i2 = Cosine_Interpolate(v3, v4, fractional_X);
+
+	return Cosine_Interpolate(i1, i2, fractional_Y);
+}
+
+float point_PerlinNoise(float x, float y, float persistence, int octaves, int ampMult){
+	float total = 0.0f;
+	float amplitude = 1;
+
+	for (int i = 0; i < octaves; i++)
+	{
+		float frequency = pow(2.0f, i);
+		amplitude *= persistence;
+		amplitude *= ampMult;
+
+		total = total + Interpolated_NoiseI(x * frequency, y * frequency) * amplitude;
+
+	}
+
+	return total;
+}
+
+void cpu_PerlinNoise(float **matrix, int w, int l, float persistence, int octaves, int ampMult){
+	double width = 0, height = 0;
+
+	for (int iz = 0; iz < l; iz++)
+	{
+		height += STEP;
+		for (int ix = 0; ix < w; ix++)
+		{
+			width += STEP;
+			matrix[iz][ix] = point_PerlinNoise(width, height, persistence, octaves, ampMult);
+		}
+	}
+}
+
 void createHeightMap(float ***height, int width, int length){
     (*height) = (float **)malloc(length*sizeof(float *));
     
@@ -325,15 +419,20 @@ int main(int argc, char **argv) {
     
     createHeightMap(&height, MAP_SIZE, MAP_SIZE);
 	
-	double dtStart = GetTime();
+	/*double dtStart = GetTime();
 	cpuFaultAlgorithm(height, MAP_SIZE, MAP_SIZE, ITERATIONS);
 	double dt = GetTime();
-	printf("all CPU time: %f\n", dt - dtStart);
+	printf("all CPUFaultAlgorithm time: %f\n", dt - dtStart);
 	
 	double dtStart2 = GetTime();
 	gpuFaultAlgorithm(height, MAP_SIZE, MAP_SIZE, ITERATIONS);
 	double dt2 = GetTime();
-	printf("all GPU time: %f\n",dt2 - dtStart2);
+	printf("all GPUFaultAlgorithm time: %f\n",dt2 - dtStart2);*/
+
+	double dtStart3 = GetTime();
+	cpu_PerlinNoise(height, MAP_SIZE, MAP_SIZE, 0.5, 8, 1);
+	double dt3 = GetTime();
+	printf("all CPUPerlinNoise time: %f\n", dt3 - dtStart3);
     
     /* Initialize GLUT state - glut will take any command line arguments that pertain to it or 
     X Windows - look at its documentation at http://reality.sgi.com/mjk/spec3/spec3.html */
@@ -368,7 +467,7 @@ int main(int argc, char **argv) {
     //glutReshapeFunc(&ReSizeGLScene);
 
     /* Register the function called when the keyboard is pressed. */
-    //glutKeyboardFunc(&keyPressed);
+	glutKeyboardFunc(&Keyboard);
 
     glutMouseFunc(mouseButton);
     glutMotionFunc(mouseMove);
